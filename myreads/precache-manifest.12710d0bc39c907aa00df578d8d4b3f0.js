246318479d6818e65a9a9ca18843cb1d5abc9ba0
root <root@vps237451.vps.ovh.ca>
2019-11-01 04:37:43 -0400

    myreads added

---
+++ myreads/precache-manifest.12710d0bc39c907aa00df578d8d4b3f0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0ea3ebdf905673c5cfe6",
+    "url": "/static/js/main.0ea3ebdf.chunk.js"
+  },
+  {
+    "revision": "e9f2db66160fac7874a3",
+    "url": "/static/js/1.e9f2db66.chunk.js"
+  },
+  {
+    "revision": "0ea3ebdf905673c5cfe6",
+    "url": "/static/css/main.b12f02a6.chunk.css"
+  },
+  {
+    "revision": "07d6af1f5cefcdf90637c824cd032984",
+    "url": "/index.html"
+  }
+];
